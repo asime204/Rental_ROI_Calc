9517d044000c37733f11dab472670e4684860f90
asime204 <azielsime1@gmail.com>
2023-01-08 15:19:06 -0500

    roi calc

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Rental_ROI_Calc
 Automated Rental Property Calculator
+
+This program calculates the return on investment (ROI) for a rental property based on various income and expense factors. It allows users to input their own data and see the resulting ROI as a percentage.
+
+DESIGN AND IMPLEMENTATION
+
+To design and implement this program, I followed these steps:
+
+I created a class called RentalProperty to represent a rental property.
+I defined the __init__ method to initialize the attributes of a RentalProperty object (e.g. total price, rental income, taxes, etc.).
+I defined four methods in the RentalProperty class: income, expenses, cash_flow, and roi. These methods calculate the total income, total expenses, total cash flow, and ROI for the property, respectively.
+I created an instance of the RentalProperty class and prompted the user to input the necessary data.
+I called the appropriate method to calculate the ROI for the property and displayed the result to the user.
+
+ADDITIONAL FEATURES
+
+I also added the ability to input and calculate the income from laundry/storage/misc sources, as well as the cost of utilities, HOA fees, lawn care, vacancy, repairs, capex, property management, and closing costs. These additional data points provide a more accurate representation of the income and expenses for the property.
+
+RUNNING THE PROGRAM
+
+To run this program, you will need Python 3 installed on your machine. No additional dependencies are required.
+
+To use the program, simply enter the requested data when prompted and the ROI will be calculated and displayed as a percentage.
---
+++ roi.py
@@ -0,0 +1,93 @@
+# Automated Rental Property Calculator
+
+class RentalProperty:
+    def __init__(self, mortgage, rental_income, amenities_income, taxes, insurance, utilities, hoa, lawn, vacancy, repairs, capex, property_management, down_payment, closing_costs, rehab_budget, misc):
+        self.mortgage = mortgage
+        self.rental_income = rental_income
+        self.amenities_income = amenities_income
+        self.taxes = taxes
+        self.insurance = insurance
+        self.utilities = utilities
+        self.hoa = hoa
+        self.lawn = lawn
+        self.vacancy = vacancy
+        self.repairs = repairs
+        self.capex = capex
+        self.property_management = property_management
+        self.down_payment = down_payment
+        self.closing_costs = closing_costs
+        self.rehab_budget = rehab_budget
+        self.misc = misc
+
+# function 1 - Income
+# rental income
+# laundry/storage/misc
+# total income = sum of all incomes
+    def income(self):
+        return self.rental_income + self.amenities_income + self.utilities
+
+# function 2 - Expenses
+# taxes
+# insurance
+# ulitilies - water/electric/sewer/garbage/gas
+# HOA
+# Lawn/Snow
+# Vacancy
+# Repairs
+# capex(fund for replacements)
+# property management
+# Mortgage
+# total expenses = sum of all expenses
+    def expenses(self):
+        return self.taxes + self.insurance + self.hoa + self.lawn + self.vacancy + self.repairs + self.capex + self.property_management + self.mortgage
+
+# funtion 3 - Cash Flow
+# total income - total expenses = total monthly cash
+    def cash_flow(self):
+        return self.income() - self.expenses()
+
+# function 4 - Cash on Cash ROI - return of cash flow
+# down paymanet
+# closing cost
+# rehab budget
+# misc/other
+# total investment
+    def roi(self):
+        annual_cash_flow = (self.cash_flow() * 12)
+        total_investment = self.down_payment + self.closing_costs + self.rehab_budget + self.misc
+        return (annual_cash_flow / total_investment)
+
+# total monthly cash X 12 months = annual cashflow
+# cash on cash = annual cash/total investment
+
+# ROI should return = cash on cash percentage
+
+rental_income = int(input("Enter rental income: "))
+amenities_income = int(input("Enter income from amenities (laundry/storage/misc): "))
+utilities = int(
+    input("Enter total costs for tenent (water/electric/sewer/garbage/gas): "))
+taxes = int(input("Enter taxes charged: "))
+insurance = int(input("Enter insurance charged: "))
+hoa = int(input("Enter HOA cost: "))
+lawn = int(input("Enter lawn cost: "))
+vacancy = int(input("Enter vacancy cost: "))
+repairs = int(input("Enter repairs cost: "))
+capex = int(input("Enter capex funds (replacements): "))
+property_management = int(input("Enter property management cost: "))
+mortgage = int(input("Enter monthly mortgage: "))
+down_payment = int(input("Enter down payment: "))
+closing_costs = int(input("Enter closing costs: "))
+rehab_budget = int(input("Enter rehab budget: "))
+misc = int(input("Enter misc/other: "))
+
+property = RentalProperty(mortgage, rental_income, amenities_income, taxes, insurance, utilities, hoa,
+                          lawn, vacancy, repairs, capex, property_management, down_payment, closing_costs, rehab_budget, misc)
+roi = property.roi()
+roi_percent = int(roi * 100)
+
+
+print(f"Total income: {property.income()}")
+print(f"Total expenses: {property.expenses()}")
+print(f"Total cash flow: {property.cash_flow()}")
+print(f"ROI: {property.roi()}")
+print(f"ROI Annual Percentage: {roi_percent}%")
